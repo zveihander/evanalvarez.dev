8c0f4bc905b4804fb11cec530f914e08f1765fe1
Evan Alvarez <lwq7e50ygv8f1w@tuta.com>
2025-12-21 17:44:04 -0600

    changed title and moved meta from app.html to +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,9 +11,16 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
-	<title>Evan Alvarez</title>
+	<title>evan alvarez</title>
 	<meta name="description" content="evan's corner of the internet" />
 	<meta name="keywords" content="programming, coding, technology, blog, portfolio" />
+	<meta name="theme-color" content="#282828" />
+
+	<meta
+		http-equiv="Content-Security-Policy"
+		content="default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; font-src 'self' data:; connect-src 'self'; img-src 'self' https: data:; object-src 'none'; base-uri 'self'"
+	/>
+	<meta http-equiv="Referrer-Policy" content="no-referrer-when-downgrade" />
 </svelte:head>
 
 <header role="navigation">
